f6450c265e60449263a3a0b5e952b3fd7a78036e
Paulo Santana <paulosantana.sa@gmail.com>
2022-04-12 23:08:50 -0300

    repositório com exercicios de vue

---
+++ data-binding/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 :style="{ textDecoration: decoration }" >Hello {{ name }}</h1>
+    <input type="text" v-model="name">
+  <br>
+  <br>
+    <a :href="link">Link para o curso!</a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    name:'Paulo',
+    link: 'https://treinamento.vuejsbrasil.org',
+    decoration: 'underline'
+
+  })
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
