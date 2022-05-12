4f516893cc6e3b39c0f753c58456e5e26ef7e58d
Paulo Santana <paulosantana.sa@gmail.com>
2022-05-12 00:39:41 -0300

    estudo de comunicação entre componentes vuejs

---
+++ udemy vuejs 2 study/componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            default: 'Anonimos'
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            this.nome = 'Pedro'
+            this.$emit('nomeMudou', this.nome)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
